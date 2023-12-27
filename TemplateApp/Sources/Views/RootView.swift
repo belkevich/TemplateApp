5d1924064990c36cb4e0d1d477e888ca80669b23
Oleksii Belkevych <belkevich.alexey@gmail.com>
2023-12-27 19:38:04 +0200

    Removed storybaords and view controllers

---
+++ TemplateApp/Sources/Views/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  TemplateApp
+//
+//  Created by Oleksii Belkevych on 27.12.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("TemplateApp")
+            .font(.system(size: 38, weight: .bold))
+            .foregroundColor(.accentColor)
+    }
+}
+
+#Preview {
+    RootView()
+}
